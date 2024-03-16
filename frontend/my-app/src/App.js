fca45127c564276f1957b97cb2fa384c19bc4daf
larasheard <ls2548@bath.ac.uk>
2024-03-16 19:25:32 +0000

    frontend and backend folders created

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { Homepage } from "./pages/homepage";
+import { LoginPage } from "./pages/login";
+import { UploadPage } from "./pages/upload";
+
+function App() {
+  return (
+    <Homepage />
+  )
+}
+
+export default App;
